e03c151f55d4fd2432377d6f5d75c9fd6bdbd263
BabylonAS <BabylonAS@yandex.ru>
2023-12-02 13:56:27 +0300

    переделан тест под нелинейное уравнение; добавлена реализация метода Solve

--- VlSU-PT3-Lab4.Tests/SolverTests.cs
+++ VlSU-PT3-Lab4.Tests/SolverTests.cs
@@ -5,9 +5,9 @@ namespace VlSU_PT3_Lab4.Tests;
 public class SolverTests
 {
     [Fact]
-    public void SolveLinearEquation()
+    public void SolveEquation()
     {
-        double result = Solver.Solve(x => 2 * x + 5, 0, -1);
-        Assert.Equal(-2.5, result, 1e-6);
+        double result = Solver.Solve(x => 2 * x * x - 5, 0, -1);
+        Assert.Equal(-Math.Sqrt(2.5), result, 1e-10);
     }
 }
--- VlSU-PT3-Lab4/Solver.cs
+++ VlSU-PT3-Lab4/Solver.cs
@@ -4,7 +4,21 @@
     {
         public static double Solve(Func<double,double> f, double x0, double x1)
         {
-            throw new NotImplementedException("Алгоритм не реализован");
+            double x, fx0 = f(x0), fx1;
+            do
+            {
+                fx1 = f(x1);
+                x = x1 - fx1 * (x0 - x1) / (fx0 - fx1);
+
+                x0 = x1; fx0 = fx1;
+                x1 = x;
+            } while (
+                Math.Abs(x0 - x1) > 2 * Double.Epsilon &&
+                Math.Abs(x0 / x1 - 1) > 2 * Double.Epsilon &&
+                Math.Abs(fx1) > 2 * Double.Epsilon
+            );
+
+            return x;
         }
     }
 }
