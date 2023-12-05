cdc73f5f8584864c0434178f81b4146793291580
BabylonAS <BabylonAS@yandex.ru>
2023-12-05 17:02:30 +0300

    добавлен метод бисекции

--- VlSU-PT3-Lab4.Tests/SolverTests.cs
+++ VlSU-PT3-Lab4.Tests/SolverTests.cs
@@ -10,4 +10,20 @@ public class SolverTests
         double result = Solver.Solve(x => 2 * x * x - 5, 0, -1);
         Assert.Equal(-Math.Sqrt(2.5), result, 1e-10);
     }
+
+    [Fact]
+    public void SolveEquationByBisection()
+    {
+        Func<double, double> f = x => 0.5*x*x*x - 6*x + 2;
+
+        // Функция имеет три корня в пределах отрезка [-4; 4].
+        // Сканируем четыре подотрезка длиной 2 каждый.
+
+        // Эталонные значения получены через библиотеку scipy
+        // для Python (функция scipy.optimize.fsolve)
+        Assert.Equal(-3.6200758584679065, Solver.SolveBisect(f, -4, -2), 1e-10);
+        Assert.Equal(Double.NaN, Solver.SolveBisect(f, -2, 0), 1e-10);
+        Assert.Equal(0.3365088035620548, Solver.SolveBisect(f, 0, 2), 1e-10);
+        Assert.Equal(3.2835670549058515, Solver.SolveBisect(f, 2, 4), 1e-10);
+    }
 }
--- VlSU-PT3-Lab4/Solver.cs
+++ VlSU-PT3-Lab4/Solver.cs
@@ -17,8 +17,58 @@
                 Math.Abs(x0 / x1 - 1) > 2 * Double.Epsilon &&
                 Math.Abs(fx1) > 2 * Double.Epsilon
             );
-
+            
             return x;
+        }
+
+        public static double SolveBisect(Func<double,double> f, double x0, double x1)
+        {
+            // Удостоверимся, что границы отрезка заданы по возрастанию
+            double a = x0, b = x1;
+            if (x1 < x0) {
+                a = x1;
+                b = x0;
+            }
+
+            // Проверить случаи, когда одна из границ является корнем
+            // функции
+            double fa = f(a), fb = f(b);
+            if (fa == 0.0)
+                return a;
+            else if (fb == 0.0)
+                return b;
+
+            // Удостоверимся, что функция принимает разные знаки на
+            // обоих границах отрезка
+            if (Math.Sign(fa) == Math.Sign(fb))
+                return Double.NaN;
+
+            // Производим деление отрезков
+            double c, fc;
+            ushort iterations = 0;
+            while (iterations < UInt16.MaxValue)
+            {
+                // Определяем середину отрезка
+                c = a + (b - a) * 0.5;
+                fc = f(c);
+
+                if (fc == 0.0 || b - a <= 2 * Double.Epsilon)
+                    // Корень найден
+                    return c;
+                else if (Math.Sign(fa) != Math.Sign(fc))
+                    // Корень находится в левом подотрезке
+                    b = c;
+                else
+                {
+                    // Корень находится в правом подотрезке
+                    a = c;
+                    fa = fc;
+                }
+                iterations++;
+            }
+
+            // Выполнено слишком много итераций
+            return Double.NaN;
         }
     }
 }
