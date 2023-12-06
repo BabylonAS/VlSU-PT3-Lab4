7c7a9fb9fde4ce47d4593f16da9a9df72c75aef7
BabylonAS <BabylonAS@yandex.ru>
2023-12-06 15:20:05 +0300

    тест и реализация гибридного метода (бисекции + секущей)

--- VlSU-PT3-Lab4.Tests/SolverTests.cs
+++ VlSU-PT3-Lab4.Tests/SolverTests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization;
 using Xunit;
 
 namespace VlSU_PT3_Lab4.Tests;
@@ -25,5 +26,26 @@ public class SolverTests
         Assert.Equal(Double.NaN, Solver.SolveBisect(f, -2, 0), 1e-10);
         Assert.Equal(0.3365088035620548, Solver.SolveBisect(f, 0, 2), 1e-10);
         Assert.Equal(3.2835670549058515, Solver.SolveBisect(f, 2, 4), 1e-10);
+    }
+
+    [Fact]
+    public void SolveEquationByHybridMethod()
+    {
+        // f(x) = sin^2 3x - cos x + 0.8
+        Func<double, double> f = x =>
+        {
+            double sine = Math.Sin(3 * x);
+            return sine * sine + Math.Cos(x) - 0.8;
+        };
+
+        Assert.Equal(Double.NaN, Solver.SolveHybrid(f, -3, -2), 1e-10);
+        Assert.Equal(-1.6755088421230933, Solver.SolveHybrid(f, -2, -1.5), 1e-10);
+        Assert.Equal(-1.3293426325582576, Solver.SolveHybrid(f, -1, -1.5), 1e-10);
+        Assert.Equal(-0.9013779322224053, Solver.SolveHybrid(f, -1, -0.5), 1e-10);
+        Assert.Equal(Double.NaN, Solver.SolveHybrid(f, -0.5, 0.5), 1e-10);
+        Assert.Equal(0.9013779322224053, Solver.SolveHybrid(f, 0.5, 1), 1e-10);
+        Assert.Equal(1.3293426325582576, Solver.SolveHybrid(f, 1, 1.5), 1e-10);
+        Assert.Equal(1.6755088421230933, Solver.SolveHybrid(f, 1.5, 2), 1e-10);
+        Assert.Equal(Double.NaN, Solver.SolveHybrid(f, 2, 3), 1e-10);
     }
 }
